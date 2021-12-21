3492e4f0dc8e1bdf367272f5badfb1a5f87dc904
jyryuitpro <jy.ryu.itpro@gmail.com>
2021-12-21 09:54:07 +0900

    Injecting AuthRepository, StreamProvider, AuthProvider into widget t

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,4 +1,9 @@
+import 'package:fb_auth_provider/pages/home_page.dart';
+import 'package:fb_auth_provider/pages/signin_page.dart';
+import 'package:fb_auth_provider/providers/auth/auth_provider.dart';
+import 'package:fb_auth_provider/providers/auth/auth_state.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
@@ -6,6 +11,18 @@ class SplashPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final authState = context.watch<AuthProvider>().state;
+
+    if (authState.authStatus == AuthStatus.authenticated) {
+      WidgetsBinding.instance!.addPostFrameCallback((_) {
+        Navigator.pushNamed(context, HomePage.routeName);
+      });
+    } else if (authState.authStatus == AuthStatus.unauthenticated) {
+      WidgetsBinding.instance!.addPostFrameCallback((_) {
+        Navigator.pushNamed(context, SigninPage.routeName);
+      });
+    }
+
     return Scaffold(
       body: Center(
         child: CircularProgressIndicator(),
